965cd9743eed031095dc9582f35d4d42f74a0941
obrm <obrm770@gmail.com>
2021-01-18 15:48:38 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PROT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
