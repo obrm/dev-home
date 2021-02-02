65442a89f5bca1b858d531becb46c5ca9e99a7a3
obrm <obrm770@gmail.com>
2021-02-02 20:06:40 +0200

    Fix misspelling

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PROT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
